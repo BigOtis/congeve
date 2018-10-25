d195255db07f96aef7e8c6a19a7923463e24180a
Buster <otis1017@gmail.com>
2018-10-24 20:33:22 -0400

    Added SSL
    Added SSL

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from eve import Eve
+import ssl
+
 app = Eve()
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('certificate.pem', 'key.pem')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context=context, debug=True)
